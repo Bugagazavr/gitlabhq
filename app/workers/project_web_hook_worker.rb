34e30651a28fc3f0ab076434bca958b80e9ed6fa
bugagazavr <kirik910@gmail.com>
2015-01-24 03:10:43 +0300

    Added X-GitLab-Event header for web hooks

--- app/workers/project_web_hook_worker.rb
+++ app/workers/project_web_hook_worker.rb
@@ -3,8 +3,8 @@ class ProjectWebHookWorker
 
   sidekiq_options queue: :project_web_hook
 
-  def perform(hook_id, data)
+  def perform(hook_id, data, hooks_scope)
     data = data.with_indifferent_access
-    WebHook.find(hook_id).execute(data)
+    WebHook.find(hook_id).execute(data, hooks_scope)
   end
 end
